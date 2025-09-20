afda84d40db8355d27d36d6717e32224f29709f8
Dipongkor Roy <dipongkorroy000@gmail.com>
2025-09-20 18:52:30 +0600

    tours page data fetching by home page

--- src/components/modules/homepage/HeroSection.tsx
+++ src/components/modules/homepage/HeroSection.tsx
@@ -1,8 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Logo from "@/assets/icons/Logo";
 import { Link } from "react-router";
+import { useGetDivisionsQuery } from "@/redux/features/division/division.api";
+import { useState } from "react";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 const HeroSection = () => {
+  const [selectedDivision, setSelectedDivision] = useState<string | undefined>(undefined);
+
+  const { data: divisionData, isLoading: divisionIsLoading } = useGetDivisionsQuery(undefined);
+
+  const divisionOption = divisionData?.map((item: { _id: string; name: string }) => ({ label: item.name, value: item._id }));
 
   return (
     <section className="relative overflow-hidden py-32 min-h-screen">
@@ -29,9 +37,29 @@ const HeroSection = () => {
               </p>
             </div>
             <div className="mt-6 flex justify-center gap-3">
-              <Button variant="outline" className="group">
-                <Link to="/tours">Explore</Link>
-              </Button>
+              <Select onValueChange={setSelectedDivision}>
+                <SelectTrigger className="w-[300px]">
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Divisions</SelectLabel>
+                    {divisionOption?.map((item: { value: string; label: string }) => (
+                      <SelectItem key={item.value} value={item.value}>
+                        {item.label}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              {selectedDivision && !divisionIsLoading ? (
+                <Button asChild>
+                  <Link to={`/tours?division=${selectedDivision}`}>Search</Link>
+                </Button>
+              ) : (
+                <Button disabled>Search</Button>
+              )}
             </div>
           </div>
         </div>
